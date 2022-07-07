1ba052e1c9283b3b24d021996b7477541437de9c
charmii03 <charmigangani46@gmail.com>
2022-07-07 23:44:12 +0530

    js completed

---
+++ zipZap.js
@@ -0,0 +1,9 @@
+function zipZap(str){
+    let nust = "";
+  for (let i = 0; i < str.length; i++){
+    if (str[i-1] != "z" || str[i+1] != "p"){
+      nust += str[i];
+    }
+  }
+  return nust;
+}
