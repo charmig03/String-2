734da74724a800def13782d6e8f97c723af7e92d
charmii03 <charmigangani46@gmail.com>
2022-07-07 23:29:07 +0530

    js completed

---
+++ countCode.js
@@ -0,0 +1,11 @@
+function countCode(str) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str.substring(i, i + 2) == "co" && str.substring(i + 3, i + 4) == "e") {
+      count += 1;
+    }
+  }
+
+  return count;
+}
