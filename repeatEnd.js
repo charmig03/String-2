160c3328f911a27b036a2d29533cd4fcf843f3f0
charmii03 <charmigangani46@gmail.com>
2022-07-07 23:36:40 +0530

    js completed

---
+++ repeatEnd.js
@@ -0,0 +1,11 @@
+function repeatEnd(str, n){
+  let retStr = "";
+  let sub = str.substring(str.length - n);
+
+    while (n != 0) {
+        retStr += sub;
+        n--
+    }
+
+    return retStr;
+}
